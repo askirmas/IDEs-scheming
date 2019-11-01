f91ab0ba1fd39b24e119efbcbd787c3dc29081fd
askirmas <kirmas@phystech.edu>
2019-11-01 03:18:09 +0200

    Add implementation

---
+++ docs/dist/v0.1.0/index.js
@@ -0,0 +1,71 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var glob_1 = __importDefault(require("glob"));
+if (typeof require !== 'undefined' && require.main === module) {
+    try {
+        main();
+    }
+    catch (e) {
+        console.error("\u001B[31m" + e + "\u001B[m");
+        process.exit(1);
+    }
+}
+/**
+* @param {string}path
+*/
+function main(ajv, 
+//TODO: read .gitignore and etc stuff
+ignore) {
+    if (ajv === void 0) { ajv = new (require('ajv'))({ schemaId: 'auto' }); }
+    if (ignore === void 0) { ignore = "node_modules/**"; }
+    var wsPattern = '*.code-workspace', wss = glob_1.default.sync(wsPattern);
+    if (wss.length === 0)
+        throw "No " + wsPattern + " found";
+    return wss.every(function (wsPath) {
+        var ws = readJson(wsPath);
+        if (!('settings' in ws && 'json.schemas' in ws.settings))
+            throw "Nothing to validate in " + wsPath;
+        return ws.settings['json.schemas']
+            .every(function (_a) {
+            var fileMatch = _a.fileMatch, url = _a.url;
+            if (url.startsWith('http')) {
+                console.warn('Schemas by URL is not supported yet');
+                return true;
+            }
+            var validate = ajv.compile(readJson(url));
+            return fileMatch.every(function (pattern) {
+                var paths = glob_1.default.sync(pattern.includes('/')
+                    ? pattern
+                    : "**/" + pattern, { ignore: ignore });
+                if (paths.length === 0)
+                    throw "No files under " + pattern;
+                return paths.every(function (p) {
+                    if (!validate(readJson(p)))
+                        throw [
+                            "#Schema.Error: " + ajv.errorsText(validate.errors),
+                            "path: " + p,
+                            "pattern: " + pattern,
+                            "schema: " + url
+                        ].join("\n");
+                    return true;
+                });
+            });
+        });
+    });
+}
+exports.default = main;
+function readJson(path) {
+    try {
+        return JSON.parse(
+        //@ts-ignore
+        fs_1.default.readFileSync(path));
+    }
+    catch (e) {
+        console.error(path + ":\n" + e);
+        process.exit(1);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,82 @@
+import fs from 'fs'
+import glob from 'glob'
+
+interface iCodeWorkspace {
+  settings: {
+    'json.schemas': {fileMatch: string[], url: string}[]
+  }
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+  try {
+    main()
+  } catch (e) {
+    console.error(`\x1b[31m${e}\x1b[m`)
+    process.exit(1)
+  }
+}
+
+/**
+* @param {string}path 
+*/
+export default function main(
+  ajv = new (require('ajv'))({schemaId: 'auto'}),
+  //TODO: read .gitignore and etc stuff
+  ignore: string | string[] = "node_modules/**"
+) {
+  
+  const wsPattern = '*.code-workspace'
+  , wss = glob.sync(wsPattern)
+  if (wss.length === 0)
+    throw `No ${wsPattern} found`
+
+  return wss.every(wsPath => {
+    const ws : iCodeWorkspace = readJson(wsPath)
+    if (!('settings' in ws && 'json.schemas' in ws.settings))
+      throw `Nothing to validate in ${wsPath}`
+
+    return ws.settings['json.schemas']
+    .every(({fileMatch, url}) => {
+      if (url.startsWith('http')) {
+        console.warn('Schemas by URL is not supported yet')
+        return true
+      }
+
+      const validate = ajv.compile(readJson(url))
+      return fileMatch.every(pattern => {
+        const paths = glob.sync(
+          pattern.includes('/')
+          ? pattern
+          : `**/${pattern}`,
+          { ignore }
+        )
+        
+        if (paths.length === 0)
+          throw `No files under ${pattern}`
+
+        return paths.every(p => {
+          if (!validate(readJson(p)))
+            throw [
+              `#Schema.Error: ${ajv.errorsText(validate.errors)}`,
+              `path: ${p}`,
+              `pattern: ${pattern}`,
+              `schema: ${url}`
+            ].join("\n")
+          return true
+        })
+      })
+    })
+  })
+}
+
+function readJson(path: string) {
+  try {
+    return JSON.parse(
+      //@ts-ignore
+      fs.readFileSync(path)
+      )
+    } catch (e) {
+      console.error(`${path}:\n${e}`)
+      process.exit(1)
+    }
+  }
